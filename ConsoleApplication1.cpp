8fa2f1fc10cde4807ba636c9f22a47bd20a0f5c3
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-30 16:21:38 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿// Чертоусова умножения
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	cout << "  ";
+
+	for (int i2 = 1; i2 <= m; i2++) {
+
+		cout <<  i2 << " ";
+	}
+	for (int i = 1; i <= n; i++) {
+		cout << endl;
+		cout << i << " "; 
+		for (int i2 = 1; i2 <= m; i2++) {
+			
+			cout << i * i2 << " ";
+			
+
+
+
+		}
+	}
+
+
+}
